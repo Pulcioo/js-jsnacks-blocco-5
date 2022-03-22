348170eb1ceb0166610232d0abb45afe673b3d63
Davide Pulci <davide.pulci@gmail.com>
2022-03-22 16:52:23 +0100

    fine

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,11 @@
 console.log("JS OK")
+
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+let stringaInvertita = invertiStringa('davide');
+// stampo parola invertita
+console.log(stringaInvertita)
+
+// funzione per invertire una stringa
+function invertiStringa(parola) {
+    return parola.split("").reverse().join("");
+}
