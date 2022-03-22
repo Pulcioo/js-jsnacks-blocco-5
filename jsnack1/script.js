c1bf54c4ea2be07c71038852fcf1389b79f900c1
Davide Pulci <davide.pulci@gmail.com>
2022-03-22 15:46:59 +0100

    fine

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,82 @@
 console.log("JS OK")
+
+// Crea un array di 10 oggetti che rappresentano una zucchina,
+// indicando per ognuna varietà, peso e lunghezza. 
+// Calcola quanto pesano tutte le zucchine.
+
+// Creo un array di 10 oggetti indicando *varietà, peso e lunghezza* 
+const zucchine = [
+    {
+        varietà: 'nera',
+        peso: 220,
+        lunghezza: 21,
+
+    },
+    {
+        varietà: 'romanesca',
+        peso: 100,
+        lunghezza: 14,
+
+    },
+    {
+        varietà: 'gialla',
+        peso: 180,
+        lunghezza: 18,
+
+    },
+    {
+        varietà: 'nera',
+        peso: 220,
+        lunghezza: 21,
+
+    },
+    {
+        varietà: 'romanesca',
+        peso: 100,
+        lunghezza: 14,
+
+    },
+    {
+        varietà: 'gialla',
+        peso: 180,
+        lunghezza: 18,
+
+    },
+    {
+        varietà: 'nera',
+        peso: 220,
+        lunghezza: 21,
+
+    },
+    {
+        varietà: 'romanesca',
+        peso: 100,
+        lunghezza: 14,
+
+    },
+    {
+        varietà: 'gialla',
+        peso: 180,
+        lunghezza: 18,
+
+    },
+    {
+        varietà: 'nera',
+        peso: 220,
+        lunghezza: 21,
+
+    },
+
+]
+
+
+sommaPesoZucchine();
+
+// Calcolo quanto pesano tutte le zucchine
+function sommaPesoZucchine() {
+    let somma = 0
+    for (i = 0; i < zucchine.length; i++) {
+        somma += zucchine[i].peso;
+    }
+    console.log('Le zucchine pesano:' + somma + 'g')
+}
